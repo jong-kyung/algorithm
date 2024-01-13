e890d4c910747116ef14e4c7201719bac12b11fa
이종경 <gyoung3063413@naver.com>
2024-01-13 16:48:15 +0900

    [BOJ] 구간합(누적합)

--- test/swea/구간합.py
+++ test/swea/구간합.py
@@ -1,18 +1,19 @@
-# SWEA 문제해결 기본 구간합
-
 for tc in range(1, int(input()) + 1):
     N, M = map(int, input().split())
     nums = list(map(int, input().split()))
+    S = [0]
+    s = 0
 
-    # 최초값 설정
-    max_sum, min_sum = 0, 987654321
+    for num in nums:
+        s += num
+        S.append(s)
 
-    for i in range(N-M+1):
-        # 슬라이싱 통해 구간합 구하기
-        cnt = sum(nums[i:i+M])
+    prefix_sum = []
 
-        # 최대값 및 최소값 갱신
-        max_sum = max(cnt, max_sum)
-        min_sum = min(cnt, min_sum)
+    for i in range(M, N + 1):
+        prefix_sum.append(S[i] - S[i - M])
+
+    max_sum = max(prefix_sum)
+    min_sum = min(prefix_sum)
 
     print(f'#{tc} {max_sum - min_sum}')
