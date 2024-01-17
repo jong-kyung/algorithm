96acd5d234f7803e8778eb053e53b83515f36ecd
이종경 <gyoung3063413@naver.com>
2024-01-17 14:27:00 +0900

    [class] 팰린드롬 - 순열

---
+++ class/문제풀이/팰린드롬-순열.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+from itertools import permutations
+
+for tc in range(int(input())):
+    k = int(input())
+    words = [input().rstrip() for _ in range(k)]
+
+    for word1, word2 in permutations(words, 2):
+        p = word1 + word2
+
+        if p == p[::-1]:
+            print(p)
+            exit(0) # break를 이용하면 가장 가까운 for문만 멈추기 때문에 exit을 통해 하나만 찾은 이후 파이썬파일을 강제 종료함.
+
+    print(0)
